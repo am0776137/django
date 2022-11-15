d4f65a7a90c389cd1b67f4587b06ec28b40c225a
am0776137 <am0776137@gmail.com>
2022-11-15 10:59:31 +0500

    helloworld: class based view added with template

--- helloworldproject/pages/views.py
+++ helloworldproject/pages/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.views.generic import TemplateView # for class-based views
+
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse("Hello World!")
-    
+class HomePageView(TemplateView):
+    template_name = 'home.html'
